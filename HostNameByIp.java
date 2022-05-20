c945ef6d30b5066b91278b8c2d39e3c719cafae7
Sai Thi Han <86397563+S-Thihan@users.noreply.github.com>
2022-05-20 06:30:59 +0630

    Update HostNameByIp.java
    Change Ip address to 8.8.8.8

--- HostNameByIp.java
+++ HostNameByIp.java
@@ -4,7 +4,7 @@ import java.net.UnknownHostException;
 public class HostNameByIp {
 	public static void main(String[] args) throws UnknownHostException{
 		// TODO Auto-generated method stub
-		InetAddress hostname = InetAddress.getByName("142.250.4.102");
+		InetAddress hostname = InetAddress.getByName("8.8.8.8");
 		System.out.println("[*] HostName " + hostname.getHostName());
 	}
 }
